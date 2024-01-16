acd0b591980c1ee26eda8f4a6f6e8637e973377b
Aviv Dukhovich <avivaviv11223344@gmail.com>
2024-01-16 18:35:59 +0200

    Worked on the claw new algorithm in ClawsAndWrist. didn't finished yet.

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/ClawsAndWrist.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/ClawsAndWrist.java
@@ -7,8 +7,8 @@ import com.qualcomm.robotcore.hardware.Servo;
 public class ClawsAndWrist extends LinearOpMode{
     Servo wrist;
     final double WRIST_UP_POSITION = 0.4;
-    final double WRIST_DOWN_POSITION = 0.08; // ToDo: Find the correct value
-    final double WRIST_COLLECT_POSITION = 0;// ToDo: Find the correct value
+    final double WRIST_DOWN_POSITION = 0.08;
+    final double WRIST_COLLECT_POSITION = 0;
 
     Servo left_claw;
     Servo right_claw;
@@ -17,17 +17,15 @@ public class ClawsAndWrist extends LinearOpMode{
     final double RIGHT_CLAW_CLOSED_POSITION = 0;
     final double RIGHT_CLAW_OPENED_POSITION = 1;
 
-    public void closeClaw(Servo claw, int claw_opened_position, int claw_closed_position, boolean finished_closing) {
-
+    public boolean clawClosed(Servo claw, double claw_previous_position, double claw_target_position) {
+        return claw.getPosition() == claw_previous_position;
     }
+
     @Override
     public void runOpMode() throws InterruptedException {
         wrist = hardwareMap.get(Servo.class, "wrist");
 
         wrist.setPosition(WRIST_DOWN_POSITION);
-
-        // 0 = down | 1 = up | 2 = moving down | 3 = moving up
-        int wrist_position = 0;
 
         left_claw = hardwareMap.get(Servo.class, "left_claw");
         right_claw = hardwareMap.get(Servo.class, "right_claw");
@@ -35,78 +33,42 @@ public class ClawsAndWrist extends LinearOpMode{
         left_claw.setPosition(LEFT_CLAW_OPENED_POSITION);
         right_claw.setPosition(RIGHT_CLAW_OPENED_POSITION);
 
-        // 0 = closed | 1 = open | 2 moving to close | 3 moving to open
-        int right_claw_position = 1;
-        int left_claw_position = 1;
-
-
         waitForStart();
 
-        while (opModeIsActive()) {
+        boolean right_bumper_pressed = false;
+        boolean right_claw_collecting = false;
+        boolean right_claw_closed = false;
 
+
+        double right_claw_previous_position = 0;
+
+        while (opModeIsActive()) {
+            if (gamepad1.right_bumper && !right_bumper_pressed && wrist.getPosition() == WRIST_DOWN_POSITION) {
+                right_claw_collecting = true;
+                right_bumper_pressed = true;
+            }
+
+            if (right_claw_collecting) {
+
+                if (wrist.getPosition() != WRIST_COLLECT_POSITION) {
+                    wrist.setPosition(WRIST_COLLECT_POSITION);
+                } else if (!right_claw_closed) {
+                    right_claw.setPosition(RIGHT_CLAW_CLOSED_POSITION);
+                right_claw_closed = clawClosed(right_claw, right_claw_previous_position, RIGHT_CLAW_CLOSED_POSITION);
+                }
+                else if (wrist.getPosition() != WRIST_DOWN_POSITION){
+                    wrist.setPosition(WRIST_DOWN_POSITION);
+                }
+                else {
+                    right_claw_collecting = false;
+                }
+                right_claw_previous_position = right_claw.getPosition();
+            }
+            if (!gamepad1.right_bumper) {
+                right_bumper_pressed = false;
+            }
         }
 
-//        while (opModeIsActive()) {
-//            if (gamepad1.y && wrist_position == 0) {
-//                wrist.setPosition(WRIST_UP_POSITION);
-//                wrist_position = 3;
-//            }
-//            if (gamepad1.y && wrist_position == 1) {
-//                wrist.setPosition(WRIST_DOWN_POSITION);
-//                wrist_position = 2;
-//            }
-//            if (!gamepad1.y && wrist_position == 3) {
-//                wrist_position = 1;
-//            }
-//            if (!gamepad1.y && wrist_position == 2){
-//                wrist_position = 0;
-//            }
-//
-//            if (gamepad1.right_bumper && right_claw_position == 1) {
-//                wrist.setPosition(WRIST_COLLECT_POSITION);
-//
-//                if (wrist.getPosition() == WRIST_COLLECT_POSITION) {
-//                    right_claw.setPosition(RIGHT_CLAW_CLOSED_POSITION);
-//                    right_claw_position = 2;
-//                }
-//
-//                if (right_claw.getPosition() >= RIGHT_CLAW_MINIMAL_CLOSED_POSITION) {
-//                    wrist.setPosition(WRIST_DOWN_POSITION);
-//                }
-//            }
-//            if (gamepad1.right_bumper && right_claw_position == 0) {
-//                right_claw.setPosition(RIGHT_CLAW_OPENED_POSITION);
-//                right_claw_position = 3;
-//            }
-//            if (!gamepad1.right_bumper && right_claw_position == 2) {
-//                right_claw_position = 0;
-//            }
-//            if (!gamepad1.right_bumper && right_claw_position == 3) {
-//                right_claw_position = 1;
-//            }
-//
-//            if (gamepad1.left_bumper && left_claw_position == 1) {
-//                wrist.setPosition(WRIST_COLLECT_POSITION);
-//
-//                if (wrist.getPosition() == WRIST_COLLECT_POSITION) {
-//                    left_claw.setPosition(LEFT_CLAW_CLOSED_POSITION);
-//                    left_claw_position = 2;
-//                }
-//
-//                if (left_claw.getPosition() >= LEFT_CLAW_MINIMAL_CLOSED_POSITION) {
-//                    wrist.setPosition(WRIST_DOWN_POSITION);
-//                }
-//            }
-//            if (gamepad1.left_bumper && left_claw_position == 0) {
-//                left_claw.setPosition(LEFT_CLAW_OPENED_POSITION);
-//                left_claw_position = 3;
-//            }
-//            if (!gamepad1.left_bumper && left_claw_position == 2) {
-//                left_claw_position = 0;
-//            }
-//            if (!gamepad1.left_bumper && left_claw_position == 3) {
-//                left_claw_position = 1;
-//            }
-//        }
+
     }
 }
