b9ed05794f32357de820b68354902ffaf0e47e1f
Hava <avivaviv11223344@gmail.com>
2024-01-18 15:35:00 +0200

    Programmed the arm

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Arm.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Arm.java
@@ -9,7 +9,7 @@ import org.firstinspires.ftc.robotcore.external.Telemetry;
 
 public class Arm {
     private DcMotor arm;
-    private final double POWER = 0.1;
+    private final double POWER = 0.2;
     private int hold_position = 0;
     private boolean got_position_to_hold = false;
     public Arm(DcMotor motor) {
