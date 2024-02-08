3621a4b7309810ab8ae6d96be384acb08dece877
Aviv Dukhovich <avivaviv11223344@gmail.com>
2024-02-08 09:57:53 +0200

    Fixed the wrist position

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Wrist.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Wrist.java
@@ -5,8 +5,8 @@ import com.qualcomm.robotcore.hardware.Servo;
 public class Wrist {
 
     static Servo wrist;
-    static final double WRIST_UP_POSITION = 0.5;
-    static final double WRIST_DOWN_POSITION = 0.1;
+    static final double WRIST_UP_POSITION = 0.6;
+    static final double WRIST_DOWN_POSITION = 0.32;
 
             ;
     static boolean was_Y_pressed = false;
