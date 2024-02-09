f65c6cb372d75b8435b39ccca50d6a5de912d768
Aviv Dukhovich <avivaviv11223344@gmail.com>
2024-02-09 10:18:42 +0200

    Worked on autonomous and changed the arrived position function

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/RBrun2.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/RBrun2.java
@@ -4,15 +4,16 @@ public class RBrun2 {
 
     static int phase = 1;
 
-    static final int PHASE_1_POSITION = 500;
+    static final int PHASE_1_POSITION = 100;
     static final int PHASE_2_POSITION = 200;
-    static final int PHASE_3_POSITION = -530;
-    static final int PHASE_4_POSITION = 150;
-    static final int PHASE_7_POSITION  = -400;
-    static final int PHASE_8_POSITION = -200;
-    static final int PHASE_9_POSITION = 0;
+    static final int PHASE_3_POSITION = 200;
+    static final int PHASE_4_POSITION = 30;
+    static final int PHASE_6_POSITION = -50;
+    static final int PHASE_7_POSITION  = 400;
+    static final int PHASE_8_POSITION = -50;
+    static final int PHASE_9_POSITION = -530;
     static final int PHASE_10_POSITION = -2300;
     static final int PHASE_12_POSITION = Arm.MINIMAL_HOLD_POSITION;
-    static final int PHASE_13_POSITION = -400;
-    static final int PHASE_14_POSITION = -400;
+    static final int PHASE_13_POSITION = -300;
+    static final int PHASE_14_POSITION = -700;
 }
