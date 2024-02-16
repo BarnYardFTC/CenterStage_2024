ea00c87ea46f4fa772578239482d750d4dc26b9c
Igal Shwartzman <yigalsh912@gmail.com>
2024-02-17 01:06:41 +0200

    changed macanum engine reversal

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/HardwareLocal.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/HardwareLocal.java
@@ -4,6 +4,7 @@ import com.qualcomm.robotcore.hardware.AnalogInput;
 import com.qualcomm.robotcore.hardware.HardwareMap;
 
 public class HardwareLocal {
+//    analog input
     public static HardwareMap hardwaremap;
     public static AnalogInput analogInput = hardwaremap.get(AnalogInput.class, "myanaloginput");
     public static double position = analogInput.getVoltage() / 3.3 * 360;
@@ -11,9 +12,10 @@ public class HardwareLocal {
     public static double getPosition() {
         return position;
     }
-
     public static void setPosition(double position) {
         HardwareLocal.position = position;
     }
+//    color sensor claws
+
 }
 
