756cc10afe5f76a84e3a8fe90ee0646fb3629ba9
Igal Shwartzman <yigalsh912@gmail.com>
2024-03-02 18:11:27 +0200

    made wrist go up when two pixels in

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Teleop.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Teleop.java
@@ -72,7 +72,7 @@ public class Teleop extends LinearOpMode {
         } else {
             Wrist.runWrist(gamepad1.y);
         }
-        if (HardwareLocal.pixelLeft() && HardwareLocal.pixelRight() && Wrist.getPosition() == Wrist.WRIST_DOWN_POSITION && Claws.getLeftClawPosition() == Claws.LEFT_CLAW_CLOSED_POSITION && Claws.getRightClawPosition() == Claws.RIGHT_CLAW_CLOSED_POSITION && Arm.getArm1Position() > -1700) {
+        if (HardwareLocal.pixelLeft() && HardwareLocal.pixelRight() && Wrist.getPosition() == Wrist.WRIST_DOWN_POSITION && Arm.getArm1Position() > -1700) {
             Wrist.setPosition(Wrist.WRIST_UP_POSITION);
         }
     }
