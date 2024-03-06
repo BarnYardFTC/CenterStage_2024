e6116097ee59fc4ff6c7cdd088e21485c420155f
Hava <avivaviv11223344@gmail.com>
2024-03-06 21:49:48 +0200

    deleted classes

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/AutonomousBF.java
@@ -0,0 +1,10 @@
+package org.firstinspires.ftc.teamcode;
+
+import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+
+@Autonomous (name = "Blue Back")
+
+public class AutonomousBF {
+
+}
