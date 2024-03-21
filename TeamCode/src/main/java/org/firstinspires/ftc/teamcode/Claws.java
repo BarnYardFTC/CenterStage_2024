d7dcd68522a4bfcf3615871f194642889c4a8d81
IMEO03 <avivaviv11223344@gmail.com>
2024-03-21 19:19:01 +0200

    values

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Claws.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Claws.java
@@ -8,10 +8,10 @@ public class Claws {
 // Variables
     private static Servo left_claw;
     private static Servo right_claw;
-    static final double LEFT_CLAW_CLOSED_POSITION = 0.42;
+    static final double LEFT_CLAW_CLOSED_POSITION = 0.39;
     static final double LEFT_CLAW_OPENED_POSITION = 0;
     static final double RIGHT_CLAW_CLOSED_POSITION = 0.11;
-    static final double RIGHT_CLAW_OPENED_POSITION = 0.53;
+    static final double RIGHT_CLAW_OPENED_POSITION = 0.50;
     private static boolean was_right_bumper_pressed;
     private static boolean was_left_bumper_pressed;
 
