9637f5ba24615c63cc33933d10855b0eb9bfbe08
Hava <avivaviv11223344@gmail.com>
2024-03-22 14:13:35 +0200

    .

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/RRBB.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/RRBB.java
@@ -15,15 +15,29 @@ public class RRBB extends LinearOpMode {
     public void runOpMode() {
         SampleMecanumDrive drive = new SampleMecanumDrive(hardwareMap);
 
-        Trajectory myTrajectory = drive.trajectoryBuilder(new Pose2d())
+        Trajectory Trajectory1 = drive.trajectoryBuilder(drive.getPoseEstimate())
                 .forward(10)
                 .build();
-
+        Trajectory Trajectory2 = drive.trajectoryBuilder(Trajectory1.end())
+                .strafeLeft(10)
+                .build();
+        Trajectory traj3 = drive.trajectoryBuilder(Trajectory2.end())
+                .strafeRight(10)
+                .build();
+        Trajectory traj4 = drive.trajectoryBuilder(new Pose2d(traj3.end().getX(), traj3.end().getY(), Math.toRadians(90)))
+                .lineToConstantHeading(new Vector2d(20,0))
+                .build();
 
         waitForStart();
 
         if(isStopRequested()) return;
+//
+        drive.followTrajectory(Trajectory1);
+        drive.followTrajectory(Trajectory2);
+        drive.followTrajectory(traj3);
+        drive.turn(Math.toRadians(90));
+        drive.followTrajectory(traj4);
+        drive.turn(Math.toRadians(drive.getPoseEstimate().getHeading()-90));
 
-        drive.followTrajectory(myTrajectory);
     }
 }
--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/drive/SampleMecanumDrive.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/drive/SampleMecanumDrive.java
@@ -30,15 +30,18 @@ import com.acmerobotics.roadrunner.trajectory.constraints.ProfileAccelerationCon
 import com.acmerobotics.roadrunner.trajectory.constraints.TrajectoryAccelerationConstraint;
 import com.acmerobotics.roadrunner.trajectory.constraints.TrajectoryVelocityConstraint;
 import com.qualcomm.hardware.lynx.LynxModule;
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
 import com.qualcomm.robotcore.hardware.DcMotor;
 import com.qualcomm.robotcore.hardware.DcMotorEx;
 import com.qualcomm.robotcore.hardware.DcMotorSimple;
 import com.qualcomm.robotcore.hardware.HardwareMap;
 import com.qualcomm.robotcore.hardware.IMU;
 import com.qualcomm.robotcore.hardware.PIDFCoefficients;
+import com.qualcomm.robotcore.hardware.Servo;
 import com.qualcomm.robotcore.hardware.VoltageSensor;
 import com.qualcomm.robotcore.hardware.configuration.typecontainers.MotorConfigurationType;
 
+import org.firstinspires.ftc.teamcode.Wrist;
 import org.firstinspires.ftc.teamcode.trajectorysequence.TrajectorySequence;
 import org.firstinspires.ftc.teamcode.trajectorysequence.TrajectorySequenceBuilder;
 import org.firstinspires.ftc.teamcode.trajectorysequence.TrajectorySequenceRunner;
