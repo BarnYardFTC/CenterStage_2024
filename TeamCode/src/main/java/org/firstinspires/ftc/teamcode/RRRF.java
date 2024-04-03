12bf5a0400087481f80a20d0fdbc9d299756a78a
Aviv Dukhovich <avivaviv11223344@gmail.com>
2024-04-03 13:52:31 +0300

    autonomous

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/RRRF.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/RRRF.java
@@ -1,7 +1,5 @@
 package org.firstinspires.ftc.teamcode;
 
-import com.acmerobotics.roadrunner.geometry.Pose2d;
-import com.acmerobotics.roadrunner.geometry.Vector2d;
 import com.acmerobotics.roadrunner.trajectory.Trajectory;
 import com.qualcomm.hardware.rev.RevBlinkinLedDriver;
 import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
