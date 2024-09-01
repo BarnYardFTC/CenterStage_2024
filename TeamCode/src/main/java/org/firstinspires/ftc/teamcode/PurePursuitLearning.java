b21ee10db470b0d5c02a9509ee6d863101cd3671
Hava <avivaviv11223344@gmail.com>
2024-09-01 13:38:27 +0300

    added ftcLib to gradle

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/PurePursuitLearning.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/PurePursuitLearning.java
@@ -1,5 +1,9 @@
 package org.firstinspires.ftc.teamcode;
 
+import static com.company.Robot.*;
 
 public class PurePursuitLearning {
+    public static void goToPosition(double x, double y, double movementSpeed) {
+
+    }
 }
